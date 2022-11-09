0173707e255374c41209a739f1606e4e9e70ffc6
TC1055 <thariq.azhar.ft21@mail.umy.ac.id>
2022-11-09 17:06:34 +0700

    Method for adding a node into the list

--- Singly_Linked_List/Program.cs
+++ Singly_Linked_List/Program.cs
@@ -17,6 +17,50 @@ namespace single_linked_list
             START = null;
         }
 
-        public void addNode()/
+        public void addNode()/*Method untuk menambahkan sebuah Node kedalam list*/
+        {
+            int nim;
+            string nm;
+            Console.Write("\nMasukkan nomer Mahasiswa: ");
+            nim = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\nMasukkan nama Mahasiswa: ");
+            nm = Console.ReadLine();
+            Node nodeBaru = new Node();
+            nodeBaru.noMhs = nim;
+            nodeBaru.nama = nm;
+
+            if (START == null || nim <= START.noMhs) /*Node ditambahkan sebagai node pertama*/
+            {
+                if((START != null) && (nim == START.noMhs))
+                {
+                    Console.WriteLine("\nNomer mahasiswa sama tidak diijinkan\n");
+                    return;
+                }
+                nodeBaru.next = START;
+                START = nodeBaru;
+                return;
+            }
+
+            /*Menemukan lokasi node baru didalam list*/
+
+            Node previous, current;
+            previous = START;
+            current = START;
+
+            while ((current != null) && (nim >= current.noMhs))
+            {
+                if (nim == current.noMhs)
+                {
+                    Console.WriteLine("\nNomer mahasiswa sama tidak diijinkan\n");
+                    return;
+                }
+                previous = current;
+                current = current.next;
+            }
+            /*Node baru akan ditempatkan diantara previous dan current*/
+
+            nodeBaru.next = current;
+            previous.next = nodeBaru;
+        }
     }
 }
