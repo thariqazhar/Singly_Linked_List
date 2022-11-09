18d30a3737fb38cbbf1a96908677577cfb1e4cea
TC1055 <thariq.azhar.ft21@mail.umy.ac.id>
2022-11-09 16:50:58 +0700

    Add project files.

---
+++ Singly_Linked_List/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace single_linked_list
+{
+    class Node
+    {
+        public int noMhs;
+        public string nama;
+        public Node next;
+    }
+
+    class List
+    {
+        Node START;
+        public List()
+        {
+            START = null;
+        }
+
+        public void addNode()/
+    }
+}
