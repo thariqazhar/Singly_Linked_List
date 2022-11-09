a91f9ca352f71b4b8692e57fa6e15ee4e4319c6e
TC1055 <thariq.azhar.ft21@mail.umy.ac.id>
2022-11-09 17:32:00 +0700

    Method for traversing the list

--- Singly_Linked_List/Program.cs
+++ Singly_Linked_List/Program.cs
@@ -94,5 +94,20 @@ namespace single_linked_list
             else
                 return(true);
         }
+
+        /*Method untuk Traverse/mengunjungi dan membaca isi list*/
+        public void traverse()
+        {
+            if (listEmpty())
+                Console.WriteLine("\nList kosong. \n");
+            else
+            {
+                Console.WriteLine("\nData didalam list adalah : \n");
+                Node currentNode;
+                for (currentNode = START; currentNode != null; currentNode = currentNode.next)
+                    Console.Write(currentNode.noMhs + " " + currentNode.nama + "\n");
+                Console.WriteLine();
+            }
+        }
     }
 }
