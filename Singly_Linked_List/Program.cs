36e20f086b05964dfb2a72498d87349fd94b74e5
TC1055 <thariq.azhar.ft21@mail.umy.ac.id>
2022-11-09 17:27:50 +0700

    Method for checking if the node is on the list or not

--- Singly_Linked_List/Program.cs
+++ Singly_Linked_List/Program.cs
@@ -76,5 +76,23 @@ namespace single_linked_list
                 START = START.next;
             return true;
         }
+
+        /*Method untuk menge-check apakah node yang dimaksud ada didalam list atau tidak*/
+        public bool Search(int nim,ref Node previous, ref Node current)
+        {
+            previous = START;
+            current = START;
+
+            while ((current != null) && (nim != current.noMhs))
+            {
+                previous = current;
+                current = current.next;
+            }
+
+            if (current == null)
+                return(false);
+            else
+                return(true);
+        }
     }
 }
